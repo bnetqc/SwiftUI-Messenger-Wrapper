3ab089d8e59b64e312851e04a4fbd2e21745f733
Olivier Binette <olivierb.net@gmail.com>
2025-12-21 16:35:49 -0500

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Olivier Binette on 2025-12-21.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
